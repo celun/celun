aec88ee2e61c7acb465621d7bdd16e4a4e992029
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-06 23:29:04 -0400

    mkCpio: Make discrete derivation for borrowed linux tool
    This means rebuilds of the initramfs are basically instantaneous.

--- pkgs/mkCpio/default.nix
+++ pkgs/mkCpio/default.nix
@@ -1,19 +1,42 @@
 { lib
+, runCommandNoCC
 /** Any (recent enough) mainline linux */
 , linux
+, runtimeShell
 }:
 
-{ name, list }:
+let
+  borrowedMkCpio = linux.overrideAttrs(attrs: {
+    outputs = [ "out" ];
+    buildPhase = ''
+      make -C source/usr gen_init_cpio
+    '';
+    installPhase = ''
+      mkdir -p $out
+      mkdir -p $out/usr
+      cp -rvt $out/usr source/usr/{gen_init_cpio,gen_initramfs.sh,default_cpio_list}
 
-linux.overrideAttrs(attrs: {
-  inherit name;
-  buildPhase = ''
-    make -C source/usr gen_init_cpio
-  '';
-  installPhase = ''
-    (cd source
-    usr/gen_initramfs.sh -o "$out" "${list}"
-    )
-  '';
-  fixupPhase = "";
-})
+      ${lib.concatMapStringsSep "\n" (tool: ''
+        tool=${tool}
+        wrapper=$out/bin/$(basename $tool)
+        mkdir -p $out/bin
+        cat <<EOF > $wrapper
+        #!${runtimeShell}
+        cd $out
+        exec ./$tool "\$@"
+        EOF
+        chmod +x $wrapper
+      '') [ "usr/gen_initramfs.sh" ]}
+    '';
+    fixupPhase = "";
+  });
+in
+
+{ name, list }:
+runCommandNoCC name {
+  nativeBuildInputs = [
+    borrowedMkCpio
+  ];
+} ''
+  gen_initramfs.sh -o "$out" "${list}"
+''

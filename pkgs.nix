84c1aa8b031bcb24555f9ced7f9afb21769ebfa1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-08 17:12:10 -0400

    pkgs.nix: Pin to known good revision

--- pkgs.nix
+++ pkgs.nix
@@ -1,2 +1,10 @@
-# TODO: Select a specific Nixpkgs revision
-import <nixpkgs>
+let
+  rev = "84f5a2e5e5ac352e8400c361e4e648026b636423";
+  sha256 = "1k4d0yp319d35ld3nbn92a4fmv6pcbfxb8pd652y4qk2ckmbnwsz";
+in
+import (
+  builtins.fetchTarball {
+    inherit sha256;
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  }
+)

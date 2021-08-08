77b71eb22c477acc1da39fc948e0466d3c7dcf75
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-08 17:00:01 -0400

    default.nix: Eval the smolix system

--- default.nix
+++ default.nix
@@ -1,4 +1,23 @@
-{ pkgs ? import ./pkgs.nix {
-  overlays = [(import ./pkgs)];
-} }:
-  pkgs.shenanigans
+#
+# Quick way to eval a smolix system.
+#
+# *This is not an external interface*. Do not import this file from your
+# project or configuration, as this may become opinionated in the future.
+#
+{ device ? null, configuration ? { } }@args:
+
+import ./lib/eval-with-configuration.nix (args // {
+  inherit device;
+  verbose = true;
+  configuration = {
+    imports = [
+      configuration
+      (
+        { lib, ... }:
+        {
+          smolix.system.automaticCross = lib.mkDefault true;
+        }
+      )
+    ];
+  };
+})

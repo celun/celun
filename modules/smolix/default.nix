b55b16ecdd8c792d98bb8cfed9744e8b073c8d25
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-08 16:59:09 -0400

    modules: Add smolix.system

--- modules/smolix/default.nix
+++ modules/smolix/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./eval.nix
+    ./system.nix
   ];
 }
---
+++ modules/smolix/system.nix
@@ -0,0 +1,61 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (config.smolix.eval) verbosely;
+
+  cfg = config.smolix.system;
+  inherit (config.nixpkgs) localSystem;
+
+  # The platform selected by the configuration
+  selectedPlatform = lib.systems.elaborate cfg.system;
+in
+{
+  options.smolix = {
+    system = {
+      automaticCross = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = ''
+          Enables automatic configuration of cross-compilation.
+
+          **Note** that while it is disabled by default, the default.nix at the
+          root of the project _will_ enable it.
+        '';
+      };
+      system = lib.mkOption {
+        # Known supported target types
+        type = lib.types.enum [
+          "i686-linux"
+          "x86_64-linux"
+          "armv7l-linux"
+          "aarch64-linux"
+        ];
+        description = ''
+          Defines the kind of target architecture system the device is.
+
+          By default, this will also setup cross-compilation where possible.
+        '';
+      };
+    };
+  };
+
+  config = {
+    assertions = [
+      {
+        assertion = pkgs.targetPlatform.system == cfg.system;
+        message = ''
+          pkgs.targetPlatform.system expected to be `${cfg.system}`, is `${pkgs.targetPlatform.system}`.
+            TIP: enable `smolix.system.automaticCross`, which will impurely automatically enable cross-compilation.
+        '';
+      }
+    ];
+
+    nixpkgs.crossSystem = lib.mkIf cfg.automaticCross (
+      lib.mkIf (
+        let result = selectedPlatform.system != localSystem.system; in
+          verbosely (builtins.trace "Building with crossSystem?: ${selectedPlatform.system} != ${localSystem.system} → ${if result then "we are" else "we're not"}.")
+          result
+      ) (verbosely (builtins.trace "    crossSystem: config: ${selectedPlatform.config}") selectedPlatform)
+    );
+  };
+}

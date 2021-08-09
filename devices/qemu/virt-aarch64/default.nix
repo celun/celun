36a4bf55c305cf33a2fb1a2d8000b1f9ee301e83
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-08 21:35:40 -0400

    devices: qemu/virt-aarch64: Add kernel config

--- devices/qemu/virt-aarch64/default.nix
+++ devices/qemu/virt-aarch64/default.nix
@@ -1,5 +1,26 @@
+{ config, lib, ... }:
+
 {
   hardware = {
     cpu = "generic-aarch64";
   };
+  wip.kernel.structuredConfig =
+    with lib.kernel;
+    let
+      inherit (config.wip.kernel) features;
+    in
+    lib.mkMerge [
+      (lib.mkIf (features.logo || features.vt || features.graphics) {
+        DRM = yes;
+        DRM_FBDEV_EMULATION = yes;
+        DRM_VIRTIO_GPU = yes;
+
+        # virtio gpu requires PCI
+        PCI = yes;
+        VIRTIO_MENU = yes;
+        VIRTIO_PCI = yes;
+        PCI_HOST_GENERIC = yes;
+      })
+    ]
+  ;
 }

f65c6764f751069753c113c65a543a49c5f28611
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-08 20:35:51 -0400

    modules: Add wip.output
    This is used to customize the `smolix.output` derivation according to
    the configuration.

--- modules/wip/default.nix
+++ modules/wip/default.nix
@@ -1,4 +1,5 @@
 {
   imports = [
+    ./output.nix
   ];
 }
---
+++ modules/wip/output.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkOption types;
+in
+{
+  options.wip = {
+    # All outputs the current configuration of smolix produces.
+    output = mkOption {
+      type = types.unspecified; # XXX
+    };
+  };
+
+  config.wip = {
+    # FIXME attrset for multiple outputs
+    # FIXME structuredConfig from config
+    output = pkgs.smolix.output.override({
+      kernel = pkgs.smolix.configurableLinux {
+        inherit (pkgs.linux_5_13) src version;
+        defconfig = "tinyconfig";
+        structuredConfig = {}; # XXX
+      };
+    });
+  };
+}

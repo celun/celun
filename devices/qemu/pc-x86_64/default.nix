e246cf5677d8a598ac9ffa99afd425661110047f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-08 21:35:29 -0400

    devices: qemu/pc-x86_64: Add kernel config

--- devices/qemu/pc-x86_64/default.nix
+++ devices/qemu/pc-x86_64/default.nix
@@ -1,5 +1,24 @@
+{ config, lib, ... }:
+
 {
   hardware = {
     cpu = "generic-x86_64";
   };
+  wip.kernel.features = with lib.kernel; {
+    # ACPI and PCI Required for many features
+    acpi = true;
+  };
+  wip.kernel.structuredConfig =
+    with lib.kernel;
+    let
+      inherit (config.wip.kernel) features;
+    in
+    lib.mkMerge [
+      (lib.mkIf (features.logo || features.vt || features.graphics) {
+        DRM = yes;
+        DRM_FBDEV_EMULATION = yes;
+        DRM_BOCHS = yes;
+      })
+    ]
+  ;
 }

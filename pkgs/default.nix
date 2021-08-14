a325122c30c435b6dd0151df51d114a7cfd17dd8
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-14 00:44:55 -0400

    pkgs: Drop cpio initramfs things
    We're left with the "minimal-initramfs" package still... but this is
    going away soon with the stage-1 module.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -22,24 +22,9 @@ in
           bridge_stp_helper
           request_key_helper
         ];
-        #initramfs = ''"${self.minimal-initramfs-cpio}"'';
-        #initramfs = ''"${self.minimal-initramfs-cpio-xz}"'';
       };
       minimal-initramfs = callPackage ./minimal-initramfs { };
-      minimal-initramfs-cpio = final.buildPackages.mkCpio {
-        name = self.minimal-initramfs.name + ".cpio.gz";
-        list = ''"${self.minimal-initramfs}/files.list"'';
-      };
-      minimal-initramfs-cpio-xz = final.runCommandNoCC "initramfs.cpio.xz" {
-        nativeBuildInputx = [
-          final.buildPackages.xz
-        ];
-      } ''
-        cat ${self.minimal-initramfs-cpio} | xz -9 -e --check=crc32 > $out
-      '';
-      output = callPackage ./output {
-        initramfs = self.minimal-initramfs-cpio;
-      };
+      output = callPackage ./output { };
     }
   );
 }

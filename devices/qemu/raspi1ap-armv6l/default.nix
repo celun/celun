37eb63a8c226957ff82c97aab4d5df7f5157c560
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-17 02:28:16 -0400

    qemu/raspi1ap-armv6l: Add emulated Raspberry Pi 1 support
    This mainly serves as an incomplete ARMv6 testing platform.
    
    Why incomplete? Graphics don't work at the current time.

---
+++ devices/qemu/raspi1ap-armv6l/default.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ../shared.nix
+  ];
+
+  hardware = {
+    cpu = "generic-armv6l";
+  };
+  # Broken at between 4.19 and 5.4...
+  # `Attempted to kill init! exitcode=0x0000000b`
+  # wip.kernel.package = pkgs.linux_5_4;
+  # See also: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=977126
+  wip.kernel.package = pkgs.linux_4_19;
+  wip.kernel.defconfig = "bcm2835_defconfig";
+  wip.kernel.structuredConfig =
+    with lib.kernel;
+    let
+      inherit (config.wip.kernel) features;
+    in
+    lib.mkMerge [
+      (lib.mkIf (features.logo || features.vt || features.graphics) {
+        DRM = yes;
+        DRM_FBDEV_EMULATION = yes;
+        DRM_VC4 = yes;
+      })
+    ]
+  ;
+
+  device.qemu = {
+    memorySize = 512;
+    kernelCmdline = [
+      "earlycon=pl011,0x20201000"
+      "console=ttyAMA0"
+    ];
+    qemuOptions = [
+      "-machine raspi1ap"
+      "-dtb $self/dtbs/bcm2835-rpi-b-plus.dtb"
+    ];
+  };
+}

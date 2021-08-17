b7bf19b555dc801f1db313c852e60a23513b2a4a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-08-17 16:08:28 -0400

    qemu/versatile-ARM9: Add emulated versatilepb support
    This mainly serves as an ARMv5 target.

---
+++ devices/qemu/versatile-ARM9/default.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ../shared.nix
+  ];
+
+  hardware = {
+    cpu = "generic-armv5tel";
+  };
+  wip.kernel.package = pkgs.linux_4_19;
+  wip.kernel.defconfig = "versatile_defconfig";
+  wip.kernel.structuredConfig =
+    with lib.kernel;
+    let
+      inherit (config.wip.kernel) features;
+    in
+    lib.mkMerge [
+      {
+        # Those are seemingly required to be enabled for versatile_defconfig.
+        VT_CONSOLE = yes;
+        FRAMEBUFFER_CONSOLE = yes;
+      }
+      # TODO: disable/enable kernel config according to features.
+    ]
+  ;
+
+  device.qemu = {
+    memorySize = 256;
+    kernelCmdline = [
+      "mem=${toString config.device.qemu.memorySize}M"
+    ];
+    qemuOptions = [
+      "-machine versatilepb"
+      ''-dtb "$self"/dtbs/versatile-pb.dtb''
+    ];
+  };
+}

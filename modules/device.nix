972db110b48a93d22aadd0eda17b61676db75ab9
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-09-13 01:17:50 -0400

    device: Allow providing a dtb name
    This can be used for some device-specific scripts, e.g. a script used to
    boot a system over a USB bootrom util (FEL).

--- modules/device.nix
+++ modules/device.nix
@@ -18,6 +18,14 @@ in
           Name of the device being built.
         '';
       };
+      dtbName = mkOption {
+        type = types.str;
+        internal = true;
+        description = ''
+          Name of the mainline dtb file. For arm64 (AArch64), prefix with the
+          SoC vendor folder name.
+        '';
+      };
       nameForDerivation = mkOption {
         internal = true;
         description = ''
